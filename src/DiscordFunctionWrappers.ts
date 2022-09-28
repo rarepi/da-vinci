2e73b7f8747611a56dd38c284f7c93459588807c
Shiki <elpingu42@gmail.com>
2022-09-28 23:56:32 +0200

    DiscordFunctionWrappers: return sent messages

--- src/DiscordFunctionWrappers.ts
+++ src/DiscordFunctionWrappers.ts
@@ -1,25 +1,35 @@
 import Discord from "discord.js"
 
-export async function sendChannelMessage(client: Discord.Client, channelId: string, ...message: string[]) {
-    await client.channels.fetch(channelId)
+export async function sendChannelMessage(client: Discord.Client, channelId: string, ...message: string[]) : Promise<Discord.Message<true> | undefined> {
+    return await client.channels.fetch(channelId)
         .then(channel => {
             if(channel?.isTextBased() && !channel.isDMBased()) {
-                channel.send(message.join(' ')).catch((error:any) => console.error(`[${error.code}] ${error.message}`));
+                return channel.send(message.join(' '))
+                    .catch((error:any) => {
+                        console.error(`[${error.code}] ${error.message}`);
+                        return undefined;
+                    });
             }
         }).catch((error:any) => {
             if(error.code == 10003) {
                 console.error(`Channel not found.`)
             } else console.error(`[${error.code}] ${error.message}`)
+            return undefined;
         });
 }
 
-export async function sendDirectMessage(client: Discord.Client, userId: string, ...message: string[]) {
-    await client.users.createDM(userId)
+export async function sendDirectMessage(client: Discord.Client, userId: string, ...message: string[]) : Promise<Discord.Message<false> | undefined>{
+    return await client.users.createDM(userId)
         .then(dm => {
-            dm.send(message.join(' ')).catch((error:any) => console.error(`[${error.code}] ${error.message}`));
+            return dm.send(message.join(' '))
+                .catch((error:any) => {
+                    console.error(`[${error.code}] ${error.message}`);
+                    return undefined;
+                });
         }).catch((error:any) => {
             if(error.code == 50033) {
                 console.error(`Invalid user.`)
             } else console.error(`[${error.code}] ${error.message}`)
+            return undefined;
         });
 }
