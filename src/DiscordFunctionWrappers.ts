f1c4e4293000802fc25add24f4396d4f8d568980
Shiki <elpingu42@gmail.com>
2022-10-06 14:58:08 +0200

    Add Documentation for LongTimer and DiscordFunctionWrappers

--- src/DiscordFunctionWrappers.ts
+++ src/DiscordFunctionWrappers.ts
@@ -1,5 +1,12 @@
 import Discord from "discord.js"
 
+/**
+ * Sends a message to the channel of the given id
+ * @param {Discord.Client} client Discord client instance
+ * @param {string} channelId Id of the channel the message will be sent to
+ * @param {string} message Text content of the message that will be sent
+ * @returns {Promise<Discord.Message<true> | undefined>} The sent message
+ */
 export async function sendChannelMessage(client: Discord.Client, channelId: string, ...message: string[]) : Promise<Discord.Message<true> | undefined> {
     return await client.channels.fetch(channelId)
         .then(channel => {
@@ -18,6 +25,13 @@ export async function sendChannelMessage(client: Discord.Client, channelId: stri
         });
 }
 
+/**
+ * Sends a message to the user of the given id
+ * @param {Discord.Client} client Discord client instance
+ * @param {string} userId Id of the user the message will be sent to
+ * @param {string} message Text content of the message that will be sent
+ * @returns {Promise<Discord.Message<true> | undefined>} The sent message
+ */
 export async function sendDirectMessage(client: Discord.Client, userId: string, ...message: string[]) : Promise<Discord.Message<false> | undefined>{
     return await client.users.createDM(userId)
         .then(dm => {
--- src/longTimer.ts
+++ src/longTimer.ts
@@ -1,7 +1,13 @@
 import { setTimeout } from "timers";
 
+/** The maximum amount of milliseconds supported by default JavaScript timers due to a 32 bit limitation */
 const MAX_TIMER_MS = 2147483647;
 
+/**
+ * Low precision timer using chains of timeouts to circumvent the time limitations on stock JavaScript timers
+ * 
+ * Warning: The chaining of timeouts might create some overhead time, so consider this a low precision timer.
+ */
 export class LongTimer {
     private callback: () => void;
     private ms: number;
@@ -14,10 +20,41 @@ export class LongTimer {
         this.interval = repeat ?? false;
     }
 
-    public isInterval() {
+    /**
+     * Creates a new interval timer
+     * @param {Function} callback Function to be called on each timeout
+     * @param {number} ms Time for each interval
+     * @returns {LongTimer} The new interval timer
+     */
+    public static setInterval(callback: () => void, ms?: number) : LongTimer {
+        const timer = new LongTimer(callback, ms, true);
+        timer.start();
+        return timer;
+    }
+
+    /**
+     * Creates a new single-timeout timer
+     * @param {Function} callback Function to be called on timeout
+     * @param {number} ms Time till timeout
+     * @returns {LongTimer} The new single-timeout timer
+     */
+    public static setTimeout(callback: () => void, ms?: number) : LongTimer {
+        const timer = new LongTimer(callback, ms, false);
+        timer.start();
+        return timer;
+    }
+
+    /**
+     * Returns whether or not this timer is an repeating interval
+     * @returns {boolean} Whether or not this timer is an repeating interval
+     */
+    public isInterval() : boolean {
         return this.interval;
     }
 
+    /**
+     * Starts the timer
+     */
     public start() {
         // use default timer functions if time is in range
         if(this.ms <= MAX_TIMER_MS) {
@@ -37,11 +74,17 @@ export class LongTimer {
         }
     }
 
+    /**
+     * Cancels the timer, then restarts it with the initial parameters
+     */
     public restart() {
         this.cancel();
         this.start();
     }
 
+    /**
+     * Cancels the timer
+     */
     public cancel() {
         if(this.interval && this.ms <= MAX_TIMER_MS)
             clearInterval(this.timeout);
@@ -49,12 +92,18 @@ export class LongTimer {
             clearTimeout(this.timeout);
     }
 
+    /**
+     * Starts a repeating interval using a recursive chain of timeouts
+     * @param {number} ms Time till timeout
+     */
     private startInterval(ms: number) {
+        // if time is within limitations, execute the callback on timeout and restart the timer
         if(ms <= MAX_TIMER_MS) {
             this.timeout = global.setTimeout(() => {
-                this.callback();
                 this.restart();
+                this.callback();
             }, ms);
+        // if time exceeds limitations, chain max length timeouts until it does not
         } else {
             this.timeout = global.setTimeout(() => {
                 this.startTimeout(ms - MAX_TIMER_MS)
@@ -62,11 +111,17 @@ export class LongTimer {
         }
     }
 
+    /**
+     * Starts a single timeout using a recursive chain of timeouts if necessary
+     * @param {number} ms Time till timeout
+     */
     private startTimeout(ms: number) {
+        // if time is within limitations, execute the callback on timeout
         if(ms <= MAX_TIMER_MS) {
-            this.timeout = setTimeout(this.callback, ms);
+            this.timeout = global.setTimeout(this.callback, ms);
+        // if time exceeds limitations, chain max length timeouts until it does not
         } else {
-            this.timeout = setTimeout(() => {
+            this.timeout = global.setTimeout(() => {
                 this.startTimeout(ms - MAX_TIMER_MS)
             }, MAX_TIMER_MS);
         }
