9c96b608b88b7944ba6114430d058098e2f0736f
Shiki <elpingu42@gmail.com>
2022-10-09 22:10:19 +0200

    Add documentation for CLI

--- src/commandline.ts
+++ src/commandline.ts
@@ -3,10 +3,15 @@ import { clientId, token } from '../config.json';
 import { ClientWithCommands } from './commandType';
 import { sendDirectMessage, sendChannelMessage } from './DiscordFunctionWrappers'
 
+/** Map storing commands by aliases */
 type FunctionMap = {
     [name:string]: (...args:any) => void
 };
 
+/**
+ * Opens a listener to the stdin and executes any known command inputs
+ * @param {ClientWithCommands} client Discord client instance
+ */
 export function setupCLI(client: ClientWithCommands) {
     const stdin = process.openStdin();
     const cli = new CLI(client);
@@ -17,33 +22,58 @@ export function setupCLI(client: ClientWithCommands) {
         const args = input.splice(1);
         if(cli.callables.hasOwnProperty(command)) {
             cli.callables[command](...args);
+        } else {
+            console.error(`Unknown command: ${command}`);
         }
     });
 }
 
+/**
+ * Defines various commands callable from CLI and provides a map of aliases to call them by name
+ */
 class CLI {
     client: ClientWithCommands;
+    /**
+     * @param client Discord client instance
+     */
     constructor(client: ClientWithCommands) {
         this.client = client;
     }
 
-    private exit = (code?: number | string) => {
+    /**
+     * Sets status to "do not disturb" and terminates the process.
+     * @param {number} code Exit code to use when terminating
+     */
+    private exit = (code?: number | undefined) => {
         this.client.user?.setStatus('dnd');
         code = Number(code);
         console.info(`Shutting down...`);
         process.exit(code);
     }
 
+    /**
+     * Sends a Discord message to the channel of provided id
+     * @param {string} channelId Id of a text based Discord channel
+     * @param {string} message The text message to be sent
+     */
     private say = async (channelId: string, ...message: string[]) => {
         await sendChannelMessage(this.client, channelId, ...message);
     }
 
+    /**
+     * Sends a Discord message to the user of provided id
+     * @param {string} userId Id of a Discord user
+     * @param {string} message The text message to be sent
+     */
     private dm = async (userId: string, ...message: string[]) => {
         await sendDirectMessage(this.client, userId, ...message);
     }
 
+    /**
+     * Registers all slash commands currently stored in the client instance and thus updates the command syntax known to the Discord bot
+     */
     private registerCommands = () => {
-        const commands_json: any[] = []
+        const commands_json: any[] = [];
     
         for (const cmd of this.client.commands) {
             commands_json.push(cmd[1].data.toJSON());
@@ -57,9 +87,13 @@ class CLI {
             .catch(console.error);
     }
 
+    /**
+     * Map of all callable aliases for the defined functions
+     */
     public callables: FunctionMap = {
         quit: this.exit,
         exit: this.exit,
+        stop: this.exit,
         say: this.say,
         dm: this.dm,
         registerCommands: this.registerCommands,
